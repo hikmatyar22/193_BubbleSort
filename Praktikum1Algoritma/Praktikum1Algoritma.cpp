a501e6759b0210968d5cba8c3fe75f44a98bb8dd
hikmatyar22 <abiq5161@gmail.com>
2024-03-07 12:13:37 +0700

    Membuat Variabel array dan Variabel n

--- Praktikum1Algoritma/Praktikum1Algoritma.cpp
+++ Praktikum1Algoritma/Praktikum1Algoritma.cpp
@@ -1,2 +1,5 @@
 #include <iostream>
 using namespace std;
+
+int a[20];                           // Deklarasi array a dengan ukuran 20
+int n;                               // Deklarasi Variabel n untuk menyimpan banyaknya elemen array
